a7f1afffd9f56ea88d27aeb3a9c11e91b177f057
roy renno <royrenno@yahoo.com>
2019-03-20 05:19:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ end
    artist_count = {}
    @@artists.each do |artist|
      if artist_count[artist]
-       artist_count[artist]+=1
+       artist_count[artist]+= 1
      else
        artist_count[artist] = 1
      end
